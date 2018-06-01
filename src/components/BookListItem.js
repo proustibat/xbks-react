a55102789d445e7aeb7419450c13793ec42d6b10
Jennifer Proust <jennifer.proust@gmail.com>
2018-06-01 10:51:49 +0200

    feat(Booklist): improve style

--- src/components/BookListItem.js
+++ src/components/BookListItem.js
@@ -2,16 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 
-const BookListItem = ( { isbn, title, price, synopsis } ) => (
+const BookListItem = ( { isbn, title, price, cover } ) => (
     <Link to={ `/book/${ isbn }` } className="list-item">
-
-        <div>
-            <h3 className="list-item__title">{ title }</h3>
-            <span className="list-item__sub-title">{ synopsis[ 0 ] }</span>
-        </div>
-
-        <h3 className="list-item__data">{ numeral( price ).format( '0.00$' ) }</h3>
-
+        <span><img src={ cover } alt="" className="list-item__preview"/></span>
+        <h3 className="list-item__title">{ title }</h3>
+        <span>{ numeral( price ).format( '0.00$' ) }</span>
     </Link>
 );
 
